30b89d72e7f9891c42bad6b88f1573585100991f
Rossi <luoweiang@gmail.com>
2023-12-10 16:51:56 +0800

    update

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="stock-analysis",
+    version="0.0.1",
+    description="A stock analysis framework",
+    author="Rossi",
+    packages=find_packages(exclude=("test", "test.*", "data")),
+    include_package_data=True,
+    data_files=[
+        ("templates", ["templates/pdf.html"])
+    ]
+)
