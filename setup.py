f668ecac3fafd8584623c91cc2a37a0281a49f23
Rossi <luoweiang@gmail.com>
2023-12-10 17:15:07 +0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="stock-analysis",
+    name="stock-ana",
     version="0.0.1",
     description="A stock analysis framework",
     author="Rossi",
